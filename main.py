c16ae06aad2218dd8adf7cee4e4b19138517c720
faciolox <alviti.1868673@studenti.uniroma1.it>
2025-02-20 21:31:39 +0100

    Corretto il formato dell'output totale delle spese per utilizzare la concatenazione corretta della stringa.

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ async def get_spesa_secondo_stato(update: Update, context: CallbackContext):
                         totale.importo += spese_cc.importo
                     break
                 out += f"{spesa.descrizione} | {spesa.timestamp} | Importo: {spesa.importo}€\n"
-            out.append(f"Totale spese: {totale.importo}€")
+            out += f"Totale spese: {totale.importo}€"
             await update.message.reply_text(f"Spese:\n {out}")
             logger.info(f"{update.message.from_user.username} | 200: Spese trovate")
             return ConversationHandler.END
