1ef7cc32b20e28c81486ff2f71e1f36f739ef230
faciolox <alviti.1868673@studenti.uniroma1.it>
2025-02-20 21:34:34 +0100

    Corretto il flusso di output delle spese per gestire correttamente i casi di spese di carta di credito.

--- main.py
+++ main.py
@@ -107,8 +107,8 @@ async def get_spesa_secondo_stato(update: Update, context: CallbackContext):
                     totale = spesa
                     if spese_cc != None:
                         totale.importo += spese_cc.importo
-                    
-                out += f"{spesa.descrizione} | {spesa.timestamp} | Importo: {spesa.importo}€\n"
+                else:   
+                    out += f"{spesa.descrizione} | {spesa.timestamp} | Importo: {spesa.importo}€\n"
             out += f"Totale spese: {totale.importo}€"
             await update.message.reply_text(f"Spese:\n {out}")
             logger.info(f"{update.message.from_user.username} | 200: Spese trovate")
